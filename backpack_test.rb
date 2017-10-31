5675b37efea32cdac51a1a748932fe41032472e1
Michelle Cinello <m.cinello@gmail.com>
2017-10-31 13:02:43 -0400

    changed method name

--- backpack_test.rb
+++ backpack_test.rb
@@ -64,7 +64,7 @@ Day: monday, Weather: sunny
 - shirt
 - gym shoes
 - packed lunch"
-    assert_equal(backpack.my_func, expected)
+    assert_equal(backpack.print_summary_of_packing_list, expected)
   end
 
 end
